6a000111978d148cb96be6f73b7bd24f394a8065
IvanDev <ivanjoelacosta605@gmail.com>
2024-07-18 15:48:50 -0400

    Mostrando Alertas

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,7 @@
 <?php
     require '../includes/app.php';
     estaAutenticado();
-
+    //Importar clases
     use App\Propiedad;
     use App\Vendedor;
 
@@ -13,6 +13,7 @@
     $resultado = $_GET['resultado'] ?? null;
 
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        //Validar ID
         $id = $_POST['id'];
         $id = filter_var($id, FILTER_VALIDATE_INT);
         if($id){
@@ -35,13 +36,14 @@
 ?>
     <main class="contenedor seccion">
         <h1>Administrador de Bienes Raices</h1>
-        <?php if(intval($resultado) === 1): ?>
-            <p class="alerta exito">Creado Correctamente</p>
-        <?php elseif(intval($resultado) === 2): ?>
-            <p class="alerta exito">Actualizado Correctamente</p>
-        <?php elseif(intval($resultado) === 3): ?>
-            <p class="alerta exito">Eliminado Correctamente</p>
-        <?php endif; ?>
+
+        <?php
+            $mensaje = mostrarNotificacion(intval($resultado));
+            if($mensaje){ ?>
+                <p class="alerta exito"><?php echo s($mensaje) ?> </p>
+            <?php }
+        ?>
+
         <a href="../admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
         <a href="../admin/vendedores/crear.php" class="boton boton-amarillo">Nuevo Vendedor</a>
 
